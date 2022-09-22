10791ef4fad2957af482a0dc5e4556e7defb6e8f
csehuman <paullee20204@snu.ac.kr>
2022-09-22 10:48:38 +0700

    Connect outlet

---
+++ BasicCovidApp/CovidOverViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CovidOverViewController.swift
+//  BasicCovidApp
+//
+//  Created by Paul Lee on 2022/09/21.
+//
+
+import UIKit
+
+class CovidOverViewController: UITableViewController {
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- BasicCovidApp/ViewController.swift
+++ BasicCovidApp/ViewController.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Alamofire
+import Charts
 
+class ViewController: UIViewController {
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
